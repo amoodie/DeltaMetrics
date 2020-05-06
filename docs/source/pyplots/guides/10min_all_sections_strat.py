32f4299688d2682bc356849ef2951dbea966249b
amoodie <amoodie@rice.edu>
2020-05-06 14:30:23 -0500

    fix strat plot bug

--- docs/source/pyplots/guides/10min_all_sections_strat.py
+++ docs/source/pyplots/guides/10min_all_sections_strat.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import deltametrics as dm
 
 rcm8cube = dm.sample_data.cube.rcm8()
+rcm8cube.stratigraphy_from('eta')
 rcm8cube.register_section('demo', dm.section.StrikeSection(y=10))
 
 fig, ax = plt.subplots(6, 1, sharex=True, figsize=(8, 5))
