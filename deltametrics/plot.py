550653c80cae780d9fe9afdf3622ce65629e38c6
Andrew Moodie <amoodie@utexas.edu>
2021-02-22 09:29:28 -0600

    add sand frac as recognized variable from deltarcm

--- deltametrics/plot.py
+++ deltametrics/plot.py
@@ -230,7 +230,8 @@ class VariableSet(object):
 
         _added_list = ['net_to_gross']
         self.known_list = ['eta', 'stage', 'depth', 'discharge',
-                           'velocity', 'sedflux', 'strata_sand_frac'] + \
+                           'velocity', 'sedflux', 'strata_sand_frac',
+                           'sandfrac'] + \
                           ['x', 'y', 'time'] + _added_list
 
         self._variables = []
@@ -407,6 +408,27 @@ class VariableSet(object):
                                           label='sediment flux')
         elif type(var) is VariableInfo:
             self._sedflux = var
+        else:
+            raise TypeError
+
+    @property
+    def sandfrac(self):
+        """Sand fraction style.
+        """
+        return self._sandfrac
+
+    @sandfrac.setter
+    def sandfrac(self, var):
+        if not var:
+            sandfrac = colors.ListedColormap(
+                ['saddlebrown', 'sienna', 'goldenrod', 'gold'])
+            sandfrac.set_under('saddlebrown')
+            # bn = colors.BoundaryNorm([1e-6, 1], sandfrac.N)
+            self._sandfrac = VariableInfo('sandfrac',
+                                          cmap=sandfrac,
+                                          label='sand fraction')
+        elif type(var) is VariableInfo:
+            self._strata_sand_frac = var
         else:
             raise TypeError
 
